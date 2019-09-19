262ad7949347b319b84b0028b8d8b00ceffd2a02
Justin Bee <jbee.appz@gmail.com>
2019-09-19 19:26:38 -0400

    Update pyboard.py

--- pyboard.py
+++ pyboard.py
@@ -2,5 +2,14 @@ import pyb
 
 from pyb import Pin
 
+
 p_out = Pin('X1', Pin.OUT_PP)
 p_out.high()
+
+x=0
+
+while x==0:
+     p_out.high()
+     pyb.delay(500)
+     p_out.low()
+     pyb.delay(500)
