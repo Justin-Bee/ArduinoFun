788d8c96de04f74233f2f1c33cda76bab16d19a1
Justin Bee <jbee.appz@gmail.com>
2019-09-21 17:31:53 -0400

    Update pyboard.py

--- pyboard.py
+++ pyboard.py
@@ -1,13 +1,13 @@
 import pyb
-
+#import Pin so we can use it in the project
 from pyb import Pin
 
-
+#set p_out to Pin X1
 p_out = Pin('X1', Pin.OUT_PP)
-p_out.high()
 
+#just a variable so the loop never stops
 x=0
-
+#while loop to continually blink the led in Pin X1
 while x==0:
      p_out.high()
      pyb.delay(500)
